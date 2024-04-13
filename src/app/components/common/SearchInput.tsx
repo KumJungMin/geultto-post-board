c7e5c5944abd2db4ecae1170bbafe02f7c5e5ece
KumJungMin <rmawjdals@naver.com>
2024-04-13 21:39:16 +0900

    feat: input에 이벤트 전송 추가하기

--- src/app/components/common/SearchInput.tsx
+++ src/app/components/common/SearchInput.tsx
@@ -1,9 +1,15 @@
 'use client'
-// TODO: 애니메이션 추가하기
-// TODO: 이벤트 동작 수정하기
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
+
+import ArrowUpIcon from '../icons/ArrowUpIcon';
+
+type SearchInputProps = {
+  text?: string;
+  onChange?: (search: string) => void;
+  onSubmit?: (e: React.FormEvent<HTMLFormElement>) => void;
+};
 
 const Form = styled.form`
   position: relative;
@@ -21,10 +27,17 @@ const Input = styled.input`
   &:focus {
     outline: none;
   }
+
+  @media (max-width: 534px) {
+    width: 300px;
+  }
 `;
 
 const Button = styled.button`
   position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   right: 14px;
   top: -14px;
   width: 54px;
@@ -33,17 +46,32 @@ const Button = styled.button`
   background-color: #0459F7;
   font-size: 24px;
   color: #fff;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #0E6EF2;
+  }
+  &:active {
+    background-color: #0A5AD9;
+  }
 `;
 
-export default function SearchInput() {
+
+export default function SearchInput({ text = '', onChange = () => {}, onSubmit = () => {} }: SearchInputProps) {
   const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    setSearch(text);
+  }, [text])
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
+    onChange(e.target.value);
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    onSubmit(e);
   };
 
   return (
@@ -55,7 +83,7 @@ export default function SearchInput() {
         placeholder="‘전체보기’를 입력하면 전체 글을 불러옵니다."
       />
       <Button>
-        {'>'}
+        <ArrowUpIcon style={{ transform: 'rotate(90deg)' }} />
       </Button>
     </Form>
   );

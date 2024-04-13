3188806fa4c332d75a061313acddbcaf51d36c8f
KumJungMin <rmawjdals@naver.com>
2024-04-13 21:42:26 +0900

    feat: 스크롤시 데이터 호출하기

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,20 @@
 "use client";
 
-import SearchHeader from "../components/SearchHeader";
-import Card from "../components/Card";
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { styled } from 'styled-components';
+import { useSearchParams } from 'next/navigation';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+import type { Filter } from '../types';
+import { usePosts } from '../composables/use-posts';
+
+import Card from "../components/search/Card";
+import Loading from "../components/ui/Loading";
+import SearchHeader from "../components/common/SearchHeader";
+import ScrollToTop from '../components/ui/ScrollToTop';
+import FullToRefresh from '../components/ui/FullToRefresh';
+import NoResult from '../components/search/NoResult';
 
 
 const Container = styled.main`
@@ -11,6 +23,9 @@ const Container = styled.main`
   min-height: 100vh;
   align-items: center;
   padding: 200px 24px 24px;
+  @media (max-width: 534px) {
+    padding-top: 120px;
+  }
 `;
 
 const Grid = styled.div`
@@ -22,26 +37,84 @@ const Grid = styled.div`
   gap: 24px;
 `;
 
-const MOCK_DATA = [
-  { title: '제목1제목1제목1제목제목1제목1제목1제목1제목1제목1제목1제????1제목1제목1제목1제????제목1제목1제목1제목1제목1제목1제목1제????????목1', category: ['카테고리1', '카테고리2'], date: '2021-01-01', author: '작성자1', link: '' },
-  { title: '제목2', category: ['카테고리1', '카테고리2'], date: '2021-01-01', author: '작성자2', link: '' },
-  { title: '제aaaa목333', category: ['카테고리1', '카테고리2', 'aaa'], date: '2021-01-01', author: '작성자3', link: '' },
-  { title: '제목4444', category: ['카테고리1', '카테고리2', 'bbbbb'], date: '2021-01-01', author: '작성자4', link: '' },
-  { title: '제목555551', category: ['카테고리1', '카테고리2', 'sads', 'aaaaa'], date: '2021-01-01', author: '작성자55555', link: '' },
-]
+const EndMessage = styled.p`
+  text-align: center;
+  color : #9FA0A1;
+`;
 
 
 export default function Search() {
+  const { replace } = useRouter();
+  const searchParams = useSearchParams();
+
+  const [currKeyword, setCurrKeyword] = useState('');
+  const [currFilter, setCurrFilter] = useState<Filter>('dt');
+  const [scrollTopVisible, setScrollTopVisible] = useState(false);
+
+  const { totalCount, posts, getPosts, getMorePosts, isLoading } = usePosts();
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    const params = new URLSearchParams(searchParams);
+    const keyword: string = params.get('keyword') || '';
+    const filter = params.get('filter') as Filter || 'dt';
+
+    if (keyword) setCurrKeyword(keyword);
+    if (filter) setCurrFilter(filter);
+
+    getPosts({ keyword, filter });
+  }
+
+  function onSubmit({ keyword, filter }: { keyword: string, filter: Filter }) {
+    setCurrKeyword(keyword);
+    setCurrFilter(filter);
+    getPosts({ keyword, filter });
+
+    const params = new URLSearchParams(searchParams);
+
+    params.set('keyword', keyword);
+    params.set('filter', filter);
+
+    const newUrl = `/search?${params.toString()}`;
+    replace(newUrl);
+  }
+
+  function toScrollTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  function onScroll() {
+    setScrollTopVisible(window.scrollY > 300);
+  }
+
   return (
-    <Container>
-      <SearchHeader />
-      <Grid>
-        {
-          MOCK_DATA.map((data, index) => (
-            <Card key={index} data={data} />
-          ))
-        }
-      </Grid>
-    </Container>
+    <>
+      <InfiniteScroll
+        dataLength={posts.length || 0}
+        next={ () => getMorePosts({ keyword: currKeyword, filter: currFilter }) }
+        hasMore={ posts.length < totalCount }
+        loader={<Loading />}
+        endMessage={ <EndMessage>더 이상의 컨텐츠가 없습니다</EndMessage> }
+        onScroll={onScroll}
+        refreshFunction={fetchPosts}
+        pullDownToRefresh
+        pullDownToRefreshThreshold={50}
+        releaseToRefreshContent={ <FullToRefresh /> }
+      >
+        <Container>
+          <SearchHeader currKeyword={currKeyword} currFilter={currFilter} handleSubmit={onSubmit}/>
+            {
+              isLoading ? <Loading fullWindow /> :
+                !posts.length ? 
+                  <NoResult /> :
+                  <Grid> {posts.map((post, index) => <Card key={index} data={post} />)} </Grid>
+              }
+        </Container>
+      </InfiniteScroll>
+      { scrollTopVisible && <ScrollToTop onClick={ toScrollTop } /> }
+    </>
   );
 }

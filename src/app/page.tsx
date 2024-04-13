0749f4482b8c75cee529362d31a2b232718aa8a9
KumJungMin <rmawjdals@naver.com>
2024-04-13 20:59:14 +0900

    feat: 검색시 params 업데이트하기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import SearchHeader from './components/SearchHeader';
+import SearchHeader from './components/common/SearchHeader';
 import { styled } from 'styled-components';
+import { useRouter, useSearchParams } from 'next/navigation';
+import type { Filter } from './types';
 
 const Container = styled.main`
   display: flex;
@@ -11,11 +13,22 @@ const Container = styled.main`
   padding: 24px;
 `;
 
-
 export default function Home() {
+  const searchParams = useSearchParams();
+  const { push } = useRouter();
+
+  function updateQuery({ keyword, filter }: { keyword: string, filter: Filter }) {
+    const params = new URLSearchParams(searchParams);
+
+    params.set('keyword', keyword);
+    params.set('filter', filter);
+    const newUrl = `/search?${params.toString()}`;
+    push(newUrl);
+  }
+
   return (
     <Container>
-      <SearchHeader />
+      <SearchHeader handleSubmit={updateQuery}/>
     </Container>
   );
 }

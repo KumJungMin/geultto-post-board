b31147787c34762f7b33225d20d0693151e14cdf
KumJungMin <rmawjdals@naver.com>
2024-04-13 21:13:01 +0900

    feat: 404 페이지 추가하기

---
+++ src/app/not-found.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { styled } from 'styled-components';
+
+const Container = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 2rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  margin-top: 20px;
+  padding: 10px 20px;
+  border-radius: 5px;
+  background-color: #0459F7;
+  color: #fff;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #0E6EF2;
+  }
+  &:active {
+    background-color: #0A5AD9;
+  }
+`;
+
+
+export default function NotFound() {
+  function goHome() {
+    window.location.href = '/';
+  }
+
+	return <Container>
+    <h1> 404, 없는 페이지입니다 ;( </h1>
+    <Button onClick={ goHome }>홈으로 가기</Button>
+  </Container>
+}
